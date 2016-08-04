0a24994632d394d7063dbc641cab79f3ac40d0f9
jxltom <jxltom@gmail.com>
2016-08-04 17:27:00 +0200

    update extension name

--- mkdsupport.py
+++ mkdsupport.py
@@ -1,19 +1,13 @@
-"""Pandoc extension for Sphinx
-
-This allows you to write your docstrings in whatever markup you want, as long
-as pandoc supports it. Markdown is used by default; use the pandoc_use_parser
-config value to change it to something else.
-
-To use, place the contents of this file in your conf.py. Alternately, put it
-somewhere in your Python path (or add where it is to your Python path) and add
-'pandoc' to the `extensions` value in your conf.py.
+"""An Sphinx extension supporting for sphinx.ext.autodoc with modules containing docstrings in Markdown
 """
 
 import pypandoc
 
+
 def setup(app):
     app.add_config_value('pandoc_use_parser', 'markdown', True)
     app.connect('autodoc-process-docstring', pandoc_process)
+
 
 def pandoc_process(app, what, name, obj, options, lines):
     if not lines:
