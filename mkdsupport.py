200d114594abc56ba193ee58abbee2bf3df786ed
jxltom <jxltom@gmail.com>
2016-08-05 12:53:17 +0200

    Fix: additional \r is added in the output for each line

--- mkdsupport.py
+++ mkdsupport.py
@@ -4,13 +4,16 @@
 import pypandoc
 
 
+# Since pypandoc.convert_text will always return strings ended with \r\n, the separator should also set to it
+SEP = u'\r\n'
+
+
 def setup(app):
-    """Add extension's default value and set new function to ```autodoc-process-docstring``` event
-    """
+    """Add extension's default value and set new function to ```autodoc-process-docstring``` event"""
 
     # The 'rebuild' parameter should set as 'html' rather than 'env' since this extension needs a full rebuild of HTML
     # document
-    app.add_config_value('mkdsupport_use_parser', 'markdown', 'html')
+    app.add_config_value('mkdsupport_use_parser', 'markdown_github', 'html')
     app.connect('autodoc-process-docstring', pandoc_process)
 
 
@@ -24,12 +27,11 @@ def pandoc_process(app, what, name, obj, options, lines):
     input_format = app.config.mkdsupport_use_parser
     output_format = 'rst'
 
-    # Since pypandoc.convert_text, which will always return a unicode string, expects unicode or utf-8 encodes string
-    # and the default encoding for Sphinx is 'utf-8-sig', it is better to convert to'utf-8' to Pandoc and then convert
-    # it back after processed
-    text = '\n'.join(lines).encode('utf-8')
-    text = pypandoc.convert_text(text, output_format, format=input_format).encode('utf-8')
+    # Since default encoding for sphinx.ext.autodoc is unicode and pypandoc.convert_text, which will always return a
+    # unicode string, expects unicode or utf-8 encodes string, there is on need for dealing with coding
+    text = SEP.join(lines)
+    text = pypandoc.convert_text(text, output_format, format=input_format)
 
-    # The 'lines' in Sphinx is a list of strings
+    # The 'lines' in Sphinx is a list of strings and the value should be changed
     del lines[:]
-    lines.extend(text.split('\n'))
+    lines.extend(text.split(SEP))
